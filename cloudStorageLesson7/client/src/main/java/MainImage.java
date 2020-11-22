beea3975e59e2f2d285b254f6181eb6dac5d4dff
nikita.baliassniy <nikita.baliassniy@yandex.ru>
2020-11-22 18:06:45 +0300

    lesson8 upd, deletion of unuseful commentaries

--- cloudStorageLesson7/client/src/main/java/MainImage.java
+++ cloudStorageLesson7/client/src/main/java/MainImage.java
@@ -8,12 +8,6 @@ public class MainImage extends JPanel {
 
     Image img = new ImageIcon("client/src/main/resources/cloud1.png").getImage();
 
-//    public static void main(String[] args){
-//        JFrame fr = new JFrame();
-//        fr.setSize(500,500);
-//        fr.setVisible(true);
-//        fr.add(new MainImage());
-//    }
     public void paintComponent(Graphics g){
         g.drawImage(img, 0, 0, null);
     }
